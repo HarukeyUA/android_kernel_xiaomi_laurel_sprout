96f6e2276ffdc7b935f759a366019fa22757ae3f
Vishalcj17 <vishalcj@aospa.co>
2021-01-11 10:47:45 +0530

    drivers: touchscreen: focaltech: Move to async probe.
    - Improves boot time.
    
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>
    Change-Id: I35fe6ba1e6abadb8f2492ef81c14bec7126c55cb

--- drivers/input/touchscreen/focaltech_touch/focaltech_core.c
+++ drivers/input/touchscreen/focaltech_touch/focaltech_core.c
@@ -2065,6 +2065,7 @@ static struct i2c_driver fts_ts_driver = {
 		.name = FTS_DRIVER_NAME,
 		.owner = THIS_MODULE,
 		.of_match_table = of_match_ptr(fts_dt_match),
+		.probe_type = PROBE_PREFER_ASYNCHRONOUS,
 	},
 	.id_table = fts_ts_id,
 };
